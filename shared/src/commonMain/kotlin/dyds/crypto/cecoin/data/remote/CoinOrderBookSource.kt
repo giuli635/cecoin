a1dd51f1ee7a3efefee15c944999439e4b429e74
Joaquín Méndez <joquimendez@gmail.com>
2026-06-05 17:52:54 -0300

    refactor: remove OrderBook, CoinCap, brokers and proxy layers
    - Remove all OrderBook-related files (domain model, sources, proxies, broker)
    - Remove CoinCap price source, market model, asset mapping and proxy
    - Remove broker abstraction layer (CoinPriceSourceBroker, CoinListDataSourceBroker)
    - Remove Binance proxy layer (BinancePriceSourceProxy, BinanceCoinListDataSourceProxy)
    - Delete CryptoRepositoryImpl (will be replaced with simpler CecoinRepositoryImpl)

--- shared/src/commonMain/kotlin/dyds/crypto/cecoin/data/remote/CoinOrderBookSource.kt
+++
@@ -1,8 +0,0 @@
-package dyds.crypto.cecoin.data.remote
-
-import dyds.crypto.cecoin.domain.model.OrderBook
-import kotlinx.coroutines.flow.Flow
-
-interface CoinOrderBookSource {
-    fun observeOrderBook(symbol: String): Flow<OrderBook>
-}
