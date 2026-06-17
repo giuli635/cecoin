317db323912f903549b430b8410471c6b00fc9b0
Joaquín Méndez <joquimendez@gmail.com>
2026-06-17 11:27:09 -0300

    feat(utils): add time constants for duration calculations

---
+++ shared/src/commonMain/kotlin/dyds/crypto/cecoin/utils/TimeConstants.kt
@@ -0,0 +1,16 @@
+package dyds.crypto.cecoin.utils
+
+const val MILLIS_IN_SECOND = 1000
+const val MILLIS_IN_SECOND_LONG = 1000L
+const val MILLIS_IN_SECOND_DOUBLE = 1000.0
+const val SECONDS_IN_MINUTE = 60
+const val SECONDS_IN_HOUR = 3600
+const val HOURS_IN_DAY = 24
+
+const val MILLIS_IN_MINUTE = 60_000L
+const val MILLIS_IN_5_MIN = 300_000L
+const val MILLIS_IN_15_MIN = 900_000L
+const val MILLIS_IN_30_MIN = 1_800_000L
+const val MILLIS_IN_1_HOUR = 3_600_000L
+const val MILLIS_IN_6_HOURS = 21_600_000L
+const val MILLIS_IN_1_DAY = 86_400_000L
