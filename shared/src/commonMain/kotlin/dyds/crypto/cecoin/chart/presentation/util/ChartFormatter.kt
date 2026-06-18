c1c91bb84a940ba30efeaa71075131c25829b1cd
Agustín Paul <a.agustinpaul@gmail.com>
2026-06-18 18:06:09 -0300

    refactor(chart): remove unused timeFormatter

--- shared/src/commonMain/kotlin/dyds/crypto/cecoin/chart/presentation/util/ChartFormatter.kt
+++ shared/src/commonMain/kotlin/dyds/crypto/cecoin/chart/presentation/util/ChartFormatter.kt
@@ -1,23 +1,11 @@
 package dyds.crypto.cecoin.chart.presentation.util
 
 import com.patrykandpatrick.vico.compose.cartesian.data.CartesianValueFormatter
-import dyds.crypto.cecoin.core.utils.HOURS_IN_DAY
-import dyds.crypto.cecoin.core.utils.MILLIS_IN_SECOND
-import dyds.crypto.cecoin.core.utils.SECONDS_IN_HOUR
-import dyds.crypto.cecoin.core.utils.SECONDS_IN_MINUTE
 import dyds.crypto.cecoin.core.utils.format.pad
 import dyds.crypto.cecoin.core.utils.format.priceStr
 
 fun priceFormatter(): CartesianValueFormatter =
     CartesianValueFormatter { _, value, _ -> priceStr(value) }
-
-fun timeFormatter(): CartesianValueFormatter = CartesianValueFormatter { _, value, _ ->
-    val ms = value.toLong() + systemTimezoneOffsetMillis()
-    val sec = ms / MILLIS_IN_SECOND
-    val m = (sec / SECONDS_IN_MINUTE) % SECONDS_IN_MINUTE
-    val h = (sec / SECONDS_IN_HOUR) % HOURS_IN_DAY
-    "${h.toInt().pad(2)}:${m.toInt().pad(2)}"
-}
 
 fun dateFormatter(): CartesianValueFormatter = CartesianValueFormatter { _, value, _ ->
     val zoned = java.time.Instant.ofEpochMilli(value.toLong())
@@ -27,4 +15,4 @@ fun dateFormatter(): CartesianValueFormatter = CartesianValueFormatter { _, valu
     val h = zoned.hour.pad(2)
     val m = zoned.minute.pad(2)
     "$day/$mon $h:$m"
-}
+}
