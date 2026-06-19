c4a84f36be43fee3ea94e8195fb469efc17f5b0e
giuli635 <giulitao635@gmail.com>
2026-06-19 17:14:38 -0300

    fix: wrong Instant class used

--- shared/src/commonMain/kotlin/dyds/crypto/cecoin/chart/presentation/util/ChartFormatter.kt
+++ shared/src/commonMain/kotlin/dyds/crypto/cecoin/chart/presentation/util/ChartFormatter.kt
@@ -3,9 +3,10 @@ package dyds.crypto.cecoin.chart.presentation.util
 import com.patrykandpatrick.vico.compose.cartesian.data.CartesianValueFormatter
 import dyds.crypto.cecoin.core.utils.format.pad
 import dyds.crypto.cecoin.core.utils.format.priceStr
-import kotlinx.datetime.Instant
+import kotlin.time.Instant
 import kotlinx.datetime.TimeZone
 import kotlinx.datetime.toLocalDateTime
+import kotlinx.datetime.number
 
 fun priceFormatter(): CartesianValueFormatter =
     CartesianValueFormatter { _, value, _ -> priceStr(value) }
@@ -13,5 +14,5 @@ fun priceFormatter(): CartesianValueFormatter =
 fun dateFormatter(): CartesianValueFormatter = CartesianValueFormatter { _, value, _ ->
     val instant = Instant.fromEpochMilliseconds(value.toLong())
     val local = instant.toLocalDateTime(TimeZone.currentSystemDefault())
-    "${local.dayOfMonth.pad(2)}/${local.monthNumber.pad(2)} ${local.hour.pad(2)}:${local.minute.pad(2)}"
+    "${local.day.pad(2)}/${local.month.number.pad(2)} ${local.hour.pad(2)}:${local.minute.pad(2)}"
 }
